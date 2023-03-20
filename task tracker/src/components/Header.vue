3bcb77b2eee0588e0b9b934809e43a53a8b2d94e
adel_gafo <adelgafo@mysale.com>
2023-03-21 01:55:53 +0800

    first commit

---
+++ task tracker/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script lang="js">
+export default {
+    name:"Header",
+    props:{
+        title:String,
+    }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
