2b66ec81dcb3df580ecc97aae6819ee05262a26d
adel_gafo <adelgafo@mysale.com>
2023-03-21 02:54:33 +0800

    Add tasks and task components and style them

--- task tracker/src/components/Button.vue
+++ task tracker/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :style="{background:color}" class="btn">{{ text }}</button>
+    <button @click="onClick()" :style="{background:color}" class="btn">{{ text }}</button>
 </template>
 
 <script lang="js">
@@ -8,6 +8,11 @@ export default {
     props:{
         text:String,
         color:String
+    },
+    methods:{
+        onClick() {
+            console.log("clicked")
+        }
     }
 }
 </script>
